b4525637b8d5f621728181248dba639a1cddefde
G. M. Rafi <77846604+MohammadRafi21@users.noreply.github.com>
2026-02-05 11:06:58 +1100

    updated

---
+++ main.py
@@ -0,0 +1,13 @@
+def show_menu():
+    print("welcome to to PythonStudyBuddy")
+    print("1. Add Topic")
+    print("2. List Topics")
+    print("3. Exit")
+
+topics = {}
+
+show_menu()
+
+choice = int(input("Please Enter your choice "))
+print(f"You chose : {choice}")
+
