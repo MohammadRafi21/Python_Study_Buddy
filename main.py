57a68cbc69c6c166bbc5f7f288ea498436b6eb37
G. M. Rafi <77846604+MohammadRafi21@users.noreply.github.com>
2026-05-04 05:23:47 +1000

    updated

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ def show_menu():
     print("welcome to to PythonStudyBuddy")
     print("1. Add Topic")
     print("2. List Topics")
-    print("3. Exit")
+    print("3. Search Topics")
+    print("4. Exit")
 
 def add_topic(topics_dict):
-    topic1 = input("Enter the topic name: ")
-    descrip1 = input("Enter the topic description: ")
-    topics_dict.update({topic1: descrip1})
+    topic_name = input("Enter the topic name: ")
+    description = input("Enter the topic description: ")
+    topics_dict[topic_name] = {
+        "description": description,
+        "status": "Learning",
+        "notes": []
+    }
+    # topics_dict.update({topic_name: description})
     print("Topic added successfully!")
 def list_topics(topics_dict):
     if not topics_dict:
@@ -15,6 +21,14 @@ def list_topics(topics_dict):
     else:
         for topic, description in topics_dict.items():
             print(f"Topic: {topic}, Description: {description}")
+def search_topic(topics_dict):
+    search_key = input("Please enter the name of the topic: ")
+    if search_key in topics_dict:
+        print(topics_dict[search_key])
+    else:
+        print("Topic not found")
+
+
 
 topics = {}
 
@@ -33,6 +47,10 @@ while True:
         print("List Topic has been Selected")
         list_topics(topics)
     elif (choice == 3):
+        print("Search Topics has been Selected")
+        search_topic(topics)
+        # Implement search functionality here
+    elif (choice == 4):
         print("You have exited the Menu")
         break
     else:
