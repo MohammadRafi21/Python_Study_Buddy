2a82d0780f4c5fc35bd5f407b97a0b3880be994f
G. M. Rafi <77846604+MohammadRafi21@users.noreply.github.com>
2026-05-04 03:01:44 +1000

    updated

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ def show_menu():
     print("2. List Topics")
     print("3. Exit")
 
+def add_topic(topics_dict):
+    topic1 = input("Enter the topic name: ")
+    descrip1 = input("Enter the topic description: ")
+    topics_dict.update({topic1: descrip1})
+    print("Topic added successfully!")
+def list_topics(topics_dict):
+    if not topics_dict:
+        print("No topics available.")
+    else:
+        for topic, description in topics_dict.items():
+            print(f"Topic: {topic}, Description: {description}")
+
 topics = {}
 
 while True:
@@ -16,14 +28,10 @@ while True:
 
     if (choice == 1):
         print("Add Topic has been Selected")
-        topic1 = input("Enter the topic name: ")
-        descrip1 = input("Enter the topic description: ")
-        topics.update({topic1: descrip1})
-        print("Topic added successfully!")
+        add_topic(topics)
     elif (choice == 2):
         print("List Topic has been Selected")
-        for topic1, descrip1 in topics.items():
-            print(f"Topic: {topic1}, Description: {descrip1}")
+        list_topics(topics)
     elif (choice == 3):
         print("You have exited the Menu")
         break
